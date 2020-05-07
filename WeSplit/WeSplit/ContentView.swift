2297a647fd4de05bbf46dc531499706a4bc8c8b4
shyamala <shyamala@abservetech.com>
2020-05-07 23:32:16 +0530

    inital commit day16 of swiftUI

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Abservetech on 07/05/20.
+//  Copyright © 2020 Abservetech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
